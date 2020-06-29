fa19cfaf1a8ee720a717bd2b317e28b0593b55fc
Thân Thái <thaitv210@gmail.com>
2020-06-29 21:56:56 +0700

    [UI] Add properties dialog

--- app/src/main/java/com/example/qrapp/ui/staff/newproduct/NewProductFragment.java
+++ app/src/main/java/com/example/qrapp/ui/staff/newproduct/NewProductFragment.java
@@ -7,8 +7,16 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.example.qrapp.Callback;
 import com.example.qrapp.R;
+import com.example.qrapp.data.Property;
+import com.example.qrapp.dialog.PropertiesDialog;
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,6 +32,12 @@ public class NewProductFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        PropertiesDialog dialog = new PropertiesDialog(getContext(), null);
+        dialog.setCallback(properties -> {
+            String json = new Gson().toJson(properties);
+            Toast.makeText(getContext(), json, Toast.LENGTH_SHORT).show();
+        });
+        dialog.show();
         return inflater.inflate(R.layout.fragment_new_product, container, false);
     }
 }
